19f7060756e7d293b9351586d13a7118ec2f8573
Daisuke Maki <lestrrat+github@gmail.com>
2017-03-16 10:50:51 +0900

    we need to set both interval and timeout

--- ingress/hcupdate.sh
+++ ingress/hcupdate.sh
@@ -3,4 +3,4 @@
 exec kubectl get ingress -o json "$@" | \
     jq -Mr '.metadata.annotations."ingress.kubernetes.io/backends"' | \
     jq -Mr 'keys[]' | \
-    xargs -P 4 -n 1 -I {} gcloud compute http-health-checks update {} --check-interval 30
+    xargs -P 4 -n 1 -I {} gcloud compute http-health-checks update {} --check-interval 30 --timeout 30
